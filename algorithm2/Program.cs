a396f1912afb14dc7f228d11da4b83dd1f4189dd
Rohan <rohangazi30@gmail.com>
2023-08-24 18:24:34 +0100

    added basic cs project file

---
+++ algorithm2/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] bruh = {12, 30, 44, 234, 9, 4, 67};
+        Console.WriteLine("This is the original array: ");
+    }
+    
